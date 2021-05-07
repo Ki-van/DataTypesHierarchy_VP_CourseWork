2a691b847f0862b43c8894cc9cd660acd3de8a1b
Ki-van <kirill.ivanin00@yandex.ru>
2021-05-07 22:06:32 +0300

    done with save to xml, almost fin with load from xml

--- Architecture.cs
+++ Architecture.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace DataTypesHierarchy_VP_CourseWork
 {
@@ -128,6 +130,9 @@ namespace DataTypesHierarchy_VP_CourseWork
 
         public  bool IsValueFitType(DataType value)
         {
+            if (value == null)
+                return true;
+
             if (value.GetType() == PointerType)
                 return true;
             
@@ -136,6 +141,283 @@ namespace DataTypesHierarchy_VP_CourseWork
 
     }
 
+    public static class Serialize
+    {
+        static Dictionary<DataType, XElement> links = new();
+        static Dictionary<ulong, DataType> unresolvedPointers = new();
+        static Dictionary<ulong, DataType> pointedOns = new();
+        static ulong id = 1;
+        public static void SaveToXML(string path)
+        {
+            links.Clear();
+            XDocument xdoc = new XDocument();
+            XElement xRoot = new XElement("DataTypes");
+            foreach (DataType dataType in DataTypes.dataTypes)
+                AddXElement(xRoot, dataType);
+
+            xdoc.Add(xRoot);
+            xdoc.Save(path);
+        }
+
+        private static void AddXElement(XElement xRoot, DataType dataType)
+        {
+            XElement xNode;
+            if (links.ContainsKey(dataType))
+            {
+                xNode = links[dataType];
+            }
+            else
+            {
+                xNode = new XElement("Name");
+                links.Add(dataType, xNode);
+            }
+            Type typeDataType = dataType.GetType();
+            if (typeDataType == typeof(Number))
+            {
+                Number num = (Number)dataType;
+                xNode.Name = "Number";
+                xNode.Add(
+                    new XElement("Name", num.Name),
+                    new XElement("Size", num.Size),
+                    new XElement("real", num.real),
+                    new XElement("signed", num.signed),
+                    new XElement("Value", num.Value)
+                    );
+                xRoot.Add(xNode);
+                
+            }
+            else if (typeDataType == typeof(Character))
+            {
+                Character type = (Character)dataType;
+
+                xNode.Name = "Character";
+                xNode.Add(
+                    new XElement("Name", type.Name),
+                    new XElement("Value", type.Value)
+                    );
+                xRoot.Add(xNode);
+            }
+            else if (typeDataType == typeof(Boolean))
+            {
+                Boolean type = (Boolean)dataType;
+                xNode.Name = "Boolean";
+                xNode.Add(
+                    new XElement("Name", type.Name),
+                    new XElement("Value", type.Value)
+                    );
+                xRoot.Add(xNode);
+            }
+            else if (typeDataType == typeof(AggregateType))
+            {
+                AggregateType aggregateType = (AggregateType)dataType;
+                xNode.Name = "AggregateType";
+                xNode.Add(
+                     new XElement("Name", aggregateType.Name)
+                    );
+
+                xRoot.Add(xNode);
+
+                XElement components = new XElement("Components");
+                xNode.Add(components);
+                foreach (DataType component in aggregateType.Components)
+                    AddXElement(components, component);
+            }
+            else if (typeDataType == typeof(DependentScalar))
+            {
+                DependentScalar dependentScalar = (DependentScalar)dataType;
+                xNode.Name = "DependentScalar";
+                xNode.Add(
+                    new XElement("Name", dependentScalar.Name),
+                    new XElement("PointerType", dependentScalar.PointerType.Name)
+                    );
+                xRoot.Add(xNode);
+                
+                if(links.ContainsKey(dataType))
+                {
+                    links[dataType].Add(new XAttribute("id", id));
+                }
+                else
+                {
+                    links.Add(dataType, new XElement("Name", new XAttribute("id", id)));
+                }
+
+                xNode.Add(new XElement("Value", id));
+                id++;
+            }
+            else
+                throw new Exception("Ошибка сериализации");
+        }
+
+        public static void LoadFromXml(string path)
+        {
+            DataTypes.dataTypes.Clear();
+            XmlDocument xdoc = new XmlDocument();
+            xdoc.Load(path);
+
+            XmlElement xRoot = xdoc.DocumentElement;
+
+            if (xRoot.HasChildNodes)
+            {
+                foreach (XmlElement xNode in xRoot)
+                {
+                    DataTypes.dataTypes.Add(DeserializeNode(xNode));
+                }
+            }
+        }
+
+        private static DataType DeserializeNode(XmlElement xNode)
+        {
+            DataType dataType = null;
+            if (xNode.Name == "Number")
+            {
+                bool signed = false;
+                bool real = true;
+                uint size = 4;
+                string name = "Name";
+                decimal value = default(decimal);
+
+                foreach(XmlNode node in xNode.ChildNodes)
+                {
+                    if(node.Name == "Name")
+                    {
+                        name = node.InnerText;
+                    } 
+                    if(node.Name == "Value")
+                    {
+                        value = decimal.Parse(node.InnerText);
+                    } 
+                    if(node.Name == "real")
+                    {
+                        real = bool.Parse(node.InnerText);
+                    } 
+                    if(node.Name == "Size")
+                    {
+                        size = uint.Parse(node.InnerText);
+                    } 
+                    if(node.Name == "signed")
+                    {
+                        signed = bool.Parse(node.InnerText);
+                    }
+                }
+
+                dataType = new Number(signed, real, size, name, value);
+
+            }
+            if(xNode.Name == "Boolean")
+            {
+                string name = "Name";
+                bool value = default(bool);
+
+                foreach (XmlNode node in xNode.ChildNodes)
+                {
+                    if (node.Name == "Name")
+                    {
+                        name = node.InnerText;
+                    }
+                    if (node.Name == "Value")
+                    {
+                        value = bool.Parse(node.InnerText);
+                    }
+                }
+
+                dataType = new Boolean(name, value);
+            }
+            if(xNode.Name == "Character")
+            {
+                string name = "Name";
+                char value = default(char);
+
+                foreach (XmlNode node in xNode.ChildNodes)
+                {
+                    if (node.Name == "Name")
+                    {
+                        name = node.InnerText;
+                    }
+                    if (node.Name == "Value")
+                    {
+                        value = char.Parse(node.InnerText);
+                    }
+                }
+
+                dataType = new Character(name, value);
+            }
+            if(xNode.Name == "AggregateType")
+            {
+                string name = "Name";
+                List<DataType> components = new();
+                foreach (XmlNode node in xNode.ChildNodes)
+                {
+                    if (node.Name == "Name")
+                    {
+                        name = node.InnerText;
+                    }
+                    if(node.Name == "Components")
+                    {
+                        foreach (XmlElement childNode in node.ChildNodes)
+                        {
+                            components.Add(DeserializeNode(childNode));
+                        }
+                    }
+
+                    dataType = new AggregateType(name, components);
+                }
+            }
+            if(xNode.Name == "DependentScalar")
+            {
+                string name = "Name";
+                Type pointerType = null;
+                DataType value = null;
+                foreach(XmlNode node in xNode.ChildNodes)
+                {
+                    if (node.Name == "Name") 
+                    {
+                        name = node.InnerText;
+                    }
+
+                    if(node.Name == "PointerType")
+                    {
+                       if(node.InnerText == "Number")
+                       {
+                            pointerType = typeof(Number);
+                       }
+                        if (node.InnerText == "Character")
+                        {
+                            pointerType = typeof(Character);
+                        }
+                        if (node.InnerText == "Boolean")
+                        {
+                            pointerType = typeof(Boolean);
+                        }
+                        if (node.InnerText == "AggregateType")
+                        {
+                            pointerType = typeof(AggregateType);
+                        }
+                        if (node.InnerText == "DependentScalar")
+                        {
+                            pointerType = typeof(DependentScalar);
+                        }
+                    }
+
+                    if (node.Name == "Value")
+                    {
+                        id = ulong.Parse(node.InnerText);
+                        if (pointedOns.ContainsKey(id))
+                            value = pointedOns[id];
+                        else
+                            unresolvedPointers.Add(id, dataType);
+                    }
+                }
+                dataType = new DependentScalar(name, pointerType, value);
+            }
+
+            if (xNode.HasAttribute("id"))
+            {
+                pointedOns.Add(uint.Parse(xNode.GetAttribute("id")), dataType);
+            }
+
+            return dataType;
+        }
+    }
 
 
 }
